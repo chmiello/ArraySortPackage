49f0c5984c80bb3b7ccfe04280b14a20c8ccadab
chmiello <bartek@chmiello.pl>
2018-10-11 21:49:36 +0200

    Test: assertInstanceOf ArraySort

--- tests/ArraySortTest.php
+++ tests/ArraySortTest.php
@@ -1,8 +1,12 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use Chmiello\ArraySortPackage\ArraySort;
 
 class ArraySortTest extends TestCase
 {
-
+    public function testObjectCreate()
+    {
+        $this->assertInstanceOf(ArraySort::class, new ArraySort([]));
+    }
 }
